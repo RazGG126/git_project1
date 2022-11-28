9874ebb2aade4b6a8f3dbca5a722ef7079e20c19
RazGG126 <118054621+RazGG126@users.noreply.github.com>
2022-11-28 16:42:37 +0900

    Update program.py
    New line added

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program!')
     print('And I change it every day')
     print('And I change it again')
+    print('From git')
 
 if __name__ == '__main__':
     main()
